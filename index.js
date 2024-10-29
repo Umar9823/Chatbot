462f59d8e80387357f21d8535b44b2c75c1966fd
UMAR MUKHTAR SHAIKH <108225144+Umar9823@users.noreply.github.com>
2024-10-29 14:34:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { generateBotResponse } = require('./models/chatbot-model'); 
+const { generateBotResponse } = require('./models/chatbot-model');
 
 const app = express();
 const PORT = 3000;
-
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 
-
 app.post('/chat', (req, res) => {
     const userMessage = req.body.message;
-    const botResponse = generateBotResponse(userMessage); 
+    const botResponse = generateBotResponse(userMessage);
     res.json({ response: botResponse });
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
